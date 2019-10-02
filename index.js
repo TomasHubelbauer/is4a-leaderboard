9269b3ae7aa5a71482643d4008f7b2db6bb63573
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-10-02 09:40:23 +0200

    Use the position field instead of stars

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', async () => {
   const response = await fetch('data.json');
   const json = await response.json();
-  document.getElementById('positionSpan').textContent = json.stars;
+  document.getElementById('positionSpan').textContent = json.position;
   document.getElementById('contenderA').textContent = json.contenderName;
   document.getElementById('contenderA').href = json.contenderUrl;
   document.getElementById('gapSpan').textContent = json.gap;
